e89c0fa918a4fcc6997748cbaef312e5f377ce69
Nils Winkler <nils@nilswinkler.com>
2013-09-12 14:32:00 +0200

    Added vless alias for using vi in less/pager mode.

--- aliases/available/vim.aliases.bash
+++ aliases/available/vim.aliases.bash
@@ -2,3 +2,9 @@ cite 'about-alias'
 about-alias 'vim abbreviations'
 
 alias v='mvim --remote-tab'
+
+VLESS=$(find /usr/share/vim -name 'less.sh')
+if [ ! -z $VLESS ]; then
+    alias vless=$VLESS
+fi
+
