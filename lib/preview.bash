82205a170bd3705a6b342a95fef6f8813950151f
Nils Winkler <nils@nilswinkler.com>
2013-11-07 13:50:54 +0100

    Simplified name resolution of the theme, using _basename_ all the way.

--- lib/preview.bash
+++ lib/preview.bash
@@ -10,7 +10,7 @@ then
   THEMES="$BASH_IT/themes/*/*.theme.bash"
   for theme in $THEMES
   do
-    BASH_IT_THEME=$(basename $theme | sed "s/.theme.bash//")
+    BASH_IT_THEME=$(basename -s '.theme.bash' $theme)
     echo "
     $BASH_IT_THEME"
     echo "" | bash --init-file $BASH_IT/bash_it.sh -i
