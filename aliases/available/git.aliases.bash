9b0d1fe85f292f902966d8f02a23e56d33eb284b
Nils Winkler <nils@nilswinkler.com>
2013-11-27 17:03:09 +0100

    Added alias showing all branches.

--- aliases/available/git.aliases.bash
+++ aliases/available/git.aliases.bash
@@ -30,6 +30,7 @@ alias gdel='git branch -D'
 alias gmu='git fetch origin -v; git fetch upstream -v; git merge upstream/master'
 alias gll='git log --graph --pretty=oneline --abbrev-commit'
 alias gg="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
+alias gga="git log --graph --all --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
 alias ggs="gg --stat"
 alias gsl="git shortlog -sn"
 alias gw="git whatchanged"
