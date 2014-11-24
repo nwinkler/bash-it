096afc946eead97244eedd3e589a770d6d1cc053
Nils Winkler <nils@nilswinkler.com>
2014-11-24 17:17:44 +0100

    Removed some vim aliases that are getting in the way of using the command line vim.

--- aliases/available/vim.aliases.bash
+++ aliases/available/vim.aliases.bash
@@ -1,20 +1,10 @@
 cite 'about-alias'
 about-alias 'vim abbreviations'
 
-alias vim='gvim -b --remote-tab'
 alias v=vim
 alias vv=/usr/bin/vim
-
-case $ostype in
-  darwin*)
-    alias vim="mvim --remote-tab"
-    ;;
-  *)
-    ;;
-esac
 
 VLESS=$(find /usr/share/vim -name 'less.sh')
 if [ ! -z $VLESS ]; then
     alias vless=$VLESS
 fi
-
