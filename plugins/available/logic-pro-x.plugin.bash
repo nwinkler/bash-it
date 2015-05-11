dd329ab6fef90f6d2848318192651235cf6bd429
Nils Winkler <nils@nilswinkler.com>
2015-05-11 08:24:16 +0200

    Added plugin for backing up Logic Pro X files

---
+++ plugins/available/logic-pro-x.plugin.bash
@@ -0,0 +1,27 @@
+cite about-plugin
+about-plugin 'helper functions for Apple Logic Pro X'
+
+export LOGIC_DIR=~/Music/Logic
+export LOGIC_BACKUP_DIR=~/Dropbox/audio/backup/Logic
+
+backup-logic () {
+  about 'backs up a Logic file to Dropbox using rsync'
+  group 'logic-pro-x'
+
+  local track=$1
+
+  if [ -n "$1" ]; then
+    mkdir -p "$LOGIC_BACKUP_DIR"
+
+    cd "$LOGIC_DIR"
+
+    rsync -avrKL --progress -d --delete-excluded "$track" "$LOGIC_BACKUP_DIR/$track"
+
+    zip -FS -r "$LOGIC_BACKUP_DIR/$track.zip" "$track"
+
+    cd -
+  else
+    echo "Track name required"
+    return 1
+  fi
+}
