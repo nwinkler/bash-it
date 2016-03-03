257c166b9888f1c2737e3e2c3e5d76c3789ec822
Nils Winkler <nils@nilswinkler.com>
2016-03-03 10:02:39 +0100

    Added aliases for projectview and edit
    Also safe-guarded against missing TODO variable

--- aliases/available/todo.txt-cli.aliases.bash
+++ aliases/available/todo.txt-cli.aliases.bash
@@ -1,8 +1,14 @@
 cite 'about-alias'
 about-alias 'todo.txt-cli abbreviations'
 
+if [ -z "$TODO" ] ; then
+  TODO="todo.sh"
+fi
+
 alias tls="$TODO ls"
 alias ta="$TODO a"
 alias trm="$TODO rm"
 alias tdo="$TODO do"
 alias tpri="$TODO pri"
+alias tpv="$TODO projectview"
+alias te="$TODO edit"
