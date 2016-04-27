143e8d8048251da854ea010c464a1af4974a6d79
Nils Winkler <nils@nilswinkler.com>
2016-04-27 12:33:57 +0200

    Adjusted docker-compose parameters to latest version

--- aliases/available/docker-compose.aliases.bash
+++ aliases/available/docker-compose.aliases.bash
@@ -2,4 +2,4 @@ cite 'about-alias'
 about-alias 'docker-compose abbreviations'
 
 alias dco="docker-compose"
-alias dcofresh="docker-compose stop ; docker-compose rm -f ; docker-compose up -d ; docker-compose logs"
+alias dcofresh="docker-compose stop ; docker-compose rm -f --all ; docker-compose up -d ; docker-compose logs -f"
