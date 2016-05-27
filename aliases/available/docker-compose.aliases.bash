382257d30c2688542f56e18356e6d9ab7f375591
Nils Winkler <nils@nilswinkler.com>
2016-05-27 17:19:27 +0200

    Added docker-compose logs alias

--- aliases/available/docker-compose.aliases.bash
+++ aliases/available/docker-compose.aliases.bash
@@ -3,3 +3,4 @@ about-alias 'docker-compose abbreviations'
 
 alias dco="docker-compose"
 alias dcofresh="docker-compose stop ; docker-compose rm -f --all ; docker-compose up -d ; docker-compose logs -f"
+alias dcol="docker-compose logs -f --tail 100"
