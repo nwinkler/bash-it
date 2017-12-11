993f98fd0deef4557e6764f03ffa605ff49d03a6
Nils Winkler <nils@nilswinkler.com>
2017-12-11 11:48:31 +0100

    Allow query parameter

--- plugins/available/fzf.plugin.bash
+++ plugins/available/fzf.plugin.bash
@@ -45,6 +45,6 @@ fbr() {
   local branches branch
   branches=$(git branch --all | grep -v HEAD) &&
   branch=$(echo "$branches" |
-           fzf-tmux --preview="" -d $(( 2 + $(wc -l <<< "$branches") )) +m) &&
+           fzf-tmux --query="$1" --preview="" -d $(( 2 + $(wc -l <<< "$branches") )) +m) &&
   git checkout $(echo "$branch" | sed "s/.* //" | sed "s#remotes/[^/]*/##")
 }
