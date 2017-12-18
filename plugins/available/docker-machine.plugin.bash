0e71dd59d38d538ed9a5628978afcc29f9823714
Nils Winkler <nils@nilswinkler.com>
2017-12-18 13:58:01 +0100

    Added docker-machine-eval function

--- plugins/available/docker-machine.plugin.bash
+++ plugins/available/docker-machine.plugin.bash
@@ -3,11 +3,18 @@ about-plugin 'Helpers to get Docker setup correctly for docker-machine'
 
 [ -z ${BASH_IT_DOCKER_MACHINE+x} ] && BASH_IT_DOCKER_MACHINE='dev'
 
-# Note, this might need to be different if you use a machine other than 'dev'
 if [[ `uname -s` == "Darwin" ]]; then
-  # check if dev machine is running
-  docker-machine ls | grep --quiet "$BASH_IT_DOCKER_MACHINE.*Running"
-  if [[ "$?" = "0" ]]; then
-    eval "$(docker-machine env $BASH_IT_DOCKER_MACHINE)"
-  fi
+  function docker-machine-eval {
+    group "docker-machine"
+    about "Reloads and sets the Docker Machine settings in the local shell session. Set the machine name using the BASH_IT_DOCKER_MACHINE variable (default: dev)"
+
+    # check if dev machine is running
+    docker-machine ls | grep --quiet "$BASH_IT_DOCKER_MACHINE.*Running"
+    if [[ "$?" = "0" ]]; then
+      eval "$(docker-machine env $BASH_IT_DOCKER_MACHINE)"
+    fi
+  }
+
+  # Run it rightaway
+  docker-machine-eval
 fi
