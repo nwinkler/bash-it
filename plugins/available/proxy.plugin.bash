2eb8444d5c565590f12df2f7ea960e448b86531a
Nils Winkler <nils@nilswinkler.com>
2019-07-31 08:49:20 +0200

    Make sed replacements work for both BSD and GNU sed
    Fixes #1411

--- plugins/available/proxy.plugin.bash
+++ plugins/available/proxy.plugin.bash
@@ -437,7 +437,7 @@ ssh-disable-proxy ()
 	group 'proxy'
 
 	if [ -f ~/.ssh/config ] ; then
-		sed -e's/^.*ProxyCommand/#	ProxyCommand/' -i ""  ~/.ssh/config
+		sed -e's/^.*ProxyCommand/#	ProxyCommand/' "${BASH_IT_SED_I_PARAMETERS[@]}"  ~/.ssh/config
 		echo "Disabled SSH config proxy settings"
 	fi
 }
@@ -449,7 +449,7 @@ ssh-enable-proxy ()
 	group 'proxy'
 
 	if [ -f ~/.ssh/config ] ; then
-		sed -e's/#	ProxyCommand/	ProxyCommand/' -i ""  ~/.ssh/config
+		sed -e's/#	ProxyCommand/	ProxyCommand/' "${BASH_IT_SED_I_PARAMETERS[@]}"  ~/.ssh/config
 		echo "Enabled SSH config proxy settings"
 	fi
 }
